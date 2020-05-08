4997f5e863b752eefc154028d15d9a2ed0268696
Hyeyoon Jung <hjung@c4r5s6.42seoul.kr>
2020-05-08 16:51:31 +0900

    checks if a number is odd, even or zero

--- 42AI_bootcamp_python/day00/ex02/whois.py
+++ 42AI_bootcamp_python/day00/ex02/whois.py
@@ -2,14 +2,25 @@ import sys
 
 num = 0
 
+def error_check(str):
+    if str[0] == '-':
+        for i in str[1:]:
+            if i > '9' or i < '0':
+                return 0
+    else:
+        for i in str:
+            if i > '9' or i < '0':
+                return 0
+    return 1
+
 if len(sys.argv) != 2:
     print('ERROR')
     sys.exit()
-elif len(sys.argv) == 2:
-    for i in sys.argv[1]:
-        if i > '9' or i < '0':
-            print ('ERROR')
-            sys.exit()
+
+else:
+    if error_check(sys.argv[1]) == 0:
+        print ('ERROR')
+        sys.exit()
 
 num = int(sys.argv[1])
 
