ae65988c1091ad5b4b89d017be5a576e9b86a737
jeonghy95 <jeonghy95@gmail.com>
2020-05-21 20:05:47 +0900

    nested dictionary (pep8 checked)

--- 42AI_bootcamp_python/day00/ex06/recipe.py
+++ 42AI_bootcamp_python/day00/ex06/recipe.py
@@ -1,49 +1,50 @@
 cookbook = {
-        'sandwich' :
-            {
-                'ingredients' : ['ham', 'bread', 'cheese', 'tomatoes'],
-                'meal' : 'lunch',
-                'prep_time' : '10'
+        'sandwich': {
+                'ingredients': ['ham', 'bread', 'cheese', 'tomatoes'],
+                'meal': 'lunch',
+                'prep_time': '10'
             },
-        'cake' :
-            {
-                'ingredients' : ['flour', 'sugar', 'eggs'],
-                'meal' : 'dessert',
-                'prep_time' : '60'
+        'cake': {
+                'ingredients': ['flour', 'sugar', 'eggs'],
+                'meal': 'dessert',
+                'prep_time': '60'
             },
-        'salad' :
-            {
-                'ingredients' : ['avocado', 'arugula', 'tomatoes', 'spinach'],
-                'meal' : 'lunch',
-                'prep_time' : '15'
+        'salad': {
+                'ingredients': ['avocado', 'arugula', 'tomatoes', 'spinach'],
+                'meal': 'lunch',
+                'prep_time': '15'
             },
-        'pizza' :
-            {
-                'ingredients' : ['delete', 'in'],
-                'meal' : 'breakfast',
-                'prep_time' : '4'
+        'pizza': {
+                'ingredients': ['delete', 'in'],
+                'meal': 'breakfast',
+                'prep_time': '4'
             }
         }
 
-#print(cookbook.keys())
-#print(cookbook.values())
-#print(cookbook.items())
+# print(cookbook.keys())
+# print(cookbook.values())
+# print(cookbook.items())
+
 
 def showrecipe(dic):
     print("Recipe for cake:")
     for k, val in cookbook.get(dic).items():
         print("{} : {}".format(k, val))
 
+
 def delrecipe(dic):
     del cookbook[dic]
 
+
 def addrecipe(name, ingrds, meal, prep):
-    cookbook[name] = {'ingredients' : ingrds, 'meal' : meal, 'prep_time' : prep}
+    cookbook[name] = {'ingredients': ingrds, 'meal': meal, 'prep_time': prep}
+
 
 def allrecipe():
     print("There are {} recipes in this book".format(len(cookbook)))
     for val in cookbook:
         print(val)
+
 
 if __name__ == "__main__":
     msg = """
@@ -59,19 +60,19 @@ if __name__ == "__main__":
         num = int(input())
         if num == 1:
             r_name = input("Enter a name: ")
-            r_ingrd= input("Enter ingredients(seperated by comma): ")
+            r_ingrd = input("Enter ingredients(seperated by comma): ")
             r_meal = input("Enter a meal type: ")
             r_time = input("Enter a prep_time: ")
             addrecipe(r_name, r_ingrd.split(","), r_meal, r_time)
         elif num == 2:
-            delrecipe(input("Please enther the recipe's name you want to delete:"))
+            delrecipe(input("Enter the recipe's name you want to delete:"))
         elif num == 3:
-            showrecipe(input("Please enter the recipe's name to get its details:"))
+            showrecipe(input("Enter the recipe's name to get its details:"))
         elif num == 4:
             allrecipe()
         elif num == 5:
             print("Cookbook closed.")
             break
-        else
-            print("This option does not exist, please type the corresponding number.")
+        else:
+            print("This option does not exist, type the corresponding number.")
             print("To exit, enter 5.")
