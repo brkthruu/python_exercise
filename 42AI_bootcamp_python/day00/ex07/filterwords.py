43df1c55ac5f3c26f1cca811b71fc076c517a977
hysimok <jeonghy95@gmail.com>
2020-05-24 12:45:20 +0900

    seperate methods

--- 42AI_bootcamp_python/day00/ex07/filterwords.py
+++ 42AI_bootcamp_python/day00/ex07/filterwords.py
@@ -2,28 +2,35 @@ import sys
 import re
 
 
+def remove_punc(strin, leng):
+    result = []
+
+    for word in strin.split(' '):
+        if len(word) > int(leng):
+            word = re.sub(r'[!"#$%&\'()*+,-./:;<=>?@\[\]^_\`{|}~\\\\]',
+                          '', word)
+            result.append(word)
+    return result
+
+
 def filterwords(argv):
     if len(argv) != 3:
         print("ERROR")
         sys.exit()
-
     for i in argv[1]:
-        if i >= '0' and i <= '9':
+        if i.isdigit():
             print("ERROR")
             sys.exit()
     for i in argv[2]:
-        if i < '0' or i > '9':
+        if not i.isdigit():
             print("ERROR")
             sys.exit()
 
     strin = argv[1]
-    result = []
+    leng = argv[2]
 
-    for word in strin.split(' '):
-        if len(word) > int(argv[2]):
-            word = re.sub(r'[!"#$%&\'()*+,-./:;<=>?@\[\]^_\`{|}~\\\\]',
-                          '', word)
-            result.append(word)
+    result = remove_punc(strin, leng)
+
     return result
 
 
