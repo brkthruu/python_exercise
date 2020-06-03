cfa8499c8a9a5c39281e013eaf4b1d6c36cb972e
hysimok <jeonghy95@gmail.com>
2020-06-04 03:57:38 +0900

    lcg algorithm

--- 42AI_bootcamp_python/day01/ex03/generator.py
+++ 42AI_bootcamp_python/day01/ex03/generator.py
@@ -7,22 +7,21 @@ def lcg(seed, loop, m=2**32, a=214013, c=2531011):
 
 def generator(text, sep=" ", option=None):
     '''Option is an optional arg, sep is mandatory'''
-    if type(text)!= str:
-        print("Text should be string.")
+    if type(text)! = str:
+        print("ERROR: Text should be string.")
         return 0
     elif option not in ["shuffle", "unique", "ordered", None]:
-        print("Invalid Option.")
+        print("ERROR: Invalid Option.")
         return 0
 
     result = list()
-    if option == None:
+    if option is None:
         result = text.split(sep)
         for i in result:
             yield i
 
     elif option == "shuffle":
         result = text.split(sep)
-        new_result = list()
         for i, j in enumerate(lcg(len(result), len(result))):
             j %= len(result)
             result[i], result[j] = result[j], result[i]
