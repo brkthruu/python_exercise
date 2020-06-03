ce4ab4855216da826717315d376d8bb2c56a512f
hysimok <jeonghy95@gmail.com>
2020-06-03 10:16:36 +0900

    operator overloading

---
+++ 42AI_bootcamp_python/day01/ex02/test.py
@@ -0,0 +1,7 @@
+from vector import Vector
+
+v1 = Vector([0.0, 1.0, 2.0, 3.0])
+v2 = v1 * 5
+print(v2)
+v3 = 5 + v1
+print(v3)
--- 42AI_bootcamp_python/day01/ex02/vector.py
+++ 42AI_bootcamp_python/day01/ex02/vector.py
@@ -1,4 +1,3 @@
-import sys
 
 class Vector:
     def __init__(self, elem):
@@ -13,17 +12,24 @@ class Vector:
             for i in range(elem):
                 self.values.append(float(i))
         elif type(elem) == tuple:
+            if len(elem) != 2:
+                print("input in (int, int) format.")
+                return None
             for i in elem:
                 if not isinstance(i, int):
                     print("input in (int, int) format.")
                     return None
-            self.values = list(range(elem[0], elem[1]))
+            self.values = list()
+            for i in range(elem[0], elem[1]):
+                self.values.append(float(i))
         self.size = len(self.values)
 
+
     def __str__(self):
-        output = ""
         return "(Vector " + repr(self.values) + ")"
 
+    def __repr__(self):
+        return "(Vector " + repr(self.values) + ")"
 
     def __add__(self, operand):
         result = list()
@@ -61,4 +67,35 @@ class Vector:
     def __rsub__(self, operand):
         return self - operand
 
+    def __truediv__(self, operand):
+        result = list()
+        if isinstance(operand, int) or isinstance(operand, float):
+            for i in self.values:
+                result.append(i / operand)
+            return Vector(result)
+        else:
+            print("Can't divide with Vector with Vector.")
+            return 0
+    
+    def __rtruediv__(self, operand):
+        return self / operand
+
+    def __mul__(self, operand):
+        if isinstance(operand, int) or isinstance(operand, float):
+            result = list()
+            for i in self.values:
+                result.append(i * operand)
+            return Vector(result)
+        elif isinstance(operand, Vector):
+            if len(operand.values) != self.size:
+                print("Can't execute dot product between these two vectors.")
+                return 0
+            else:
+                result = 0
+                for i, data in enumerate(self.values):
+                    result += self.values[i] * operand.values[i]
+                return result
+
+    def __rmul__(self, operand):
+        return self * operand
 
